0f98b1ee42ae75797f2cb94f08466a6f018251f9
Amit Biswas <amitbiswas9282@gmail.com>
2024-11-14 15:15:29 +0600

    knowledge cage  first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-serif font-semibold bg-lime-500">
+        Knowledge Cafe
+      </h1>
+    </>
+  );
+}
+
+export default App;
